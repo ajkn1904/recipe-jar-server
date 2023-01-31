7d4973cd8dcecf7257ff1d9eaeb58f1886469d81
ajkn1904 <ajknishat@gmail.com>
2023-02-01 02:49:21 +0600

    api added to get recipe details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -39,6 +39,13 @@ const recipesCollection = client.db('recipeJar').collection('recipes');
             const query = {};
             const recipes = await recipesCollection.find(query).toArray()
             res.send(recipes)
+        });
+
+        app.get('/recipes/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = { _id : ObjectId(id)};
+            const result = await recipesCollection.findOne(filter);
+            res.send(result);
         });
 
     }
