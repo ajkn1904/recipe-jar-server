5aa95966378fe335d376a236aa182ee5ff612b22
ajkn1904 <ajknishat@gmail.com>
2023-02-01 20:19:41 +0600

    api added to get my recipes

--- index.js
+++ index.js
@@ -16,40 +16,48 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+//console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
     try {
-const usersCollection = client.db('recipeJar').collection('Users');
-const recipesCollection = client.db('recipeJar').collection('recipes');
+        const usersCollection = client.db('recipeJar').collection('Users');
+        const recipesCollection = client.db('recipeJar').collection('recipes');
 
 
 
-       app.post('/users', async (req, res) => {
+        app.post('/users', async (req, res) => {
             const users = req.body
             const result = await usersCollection.insertOne(users)
             res.send(result)
         });
 
 
-        app.get('/recipes', async(req, res) => {
+        app.get('/recipes', async (req, res) => {
             const query = {};
             const recipes = await recipesCollection.find(query).toArray()
             res.send(recipes)
         });
 
-        app.get('/recipes/:id', async(req, res) => {
+        app.get('/recipes/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id : ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const result = await recipesCollection.findOne(filter);
             res.send(result);
         });
 
+
+        app.get('/users/recipes', async (req, res) => {
+            const email = req.query.email;
+            const query = { userEmail: email};
+            const recipes = await recipesCollection.find(query).toArray();
+            res.send(recipes);
+        });
+
     }
-    finally{}
+    finally { }
 }
 run().catch(console.log);
 
